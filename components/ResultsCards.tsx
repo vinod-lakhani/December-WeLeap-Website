ae00dc77f3deaede796f0a829e5e00fa0c502e6d
vinod-lakhani <vinod.lakhani@gmail.com>
2026-01-20 11:17:48 -0800

    Update Market pressure text: 'Many students use roommates' -> 'Many renters use roommates'

--- components/ResultsCards.tsx
+++ components/ResultsCards.tsx
@@ -206,7 +206,7 @@ export function ResultsCards({
                 </p>
                 {hudRentRange.low > rentRangeHigh && (
                   <p className="text-xs text-[#111827]/80">
-                    Market pressure: Typical rents run higher than your safe range. Many students use roommates.
+                    Market pressure: Typical rents run higher than your safe range. Many renters use roommates.
                   </p>
                 )}
                 {hudRentRange.high < rentRangeLow && (
